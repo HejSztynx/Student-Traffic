4c1ed1b2aed99eabbf2a62a375688ced7d9f808f
tsmyda <tomasz.smyda@gmail.com>
2025-04-13 16:17:17 +0200

    final version (i guess)

--- frontend/app/app/events/football/page.jsx
+++ frontend/app/app/events/football/page.jsx
@@ -3,19 +3,19 @@
 import React from "react";
 import VerticalTimeline from "@/components/ui/eventSchedule";
 
-const reservations = [
-  {
-    title: "Gramy meczycho",
-    ownerName: "Kuba",
-    currentPlayers: 5,
-    maxPlayers: 51,
-    startTime: "14:00",
-    endTime: "16:00",
-    date: new Date("2025-04-13"),
-  },
-];
+// const reservations = [
+//   {
+//     title: "Gramy meczycho",
+//     ownerName: "Kuba",
+//     currentPlayers: 5,
+//     maxPlayers: 51,
+//     startTime: "14:00",
+//     endTime: "16:00",
+//     date: new Date("2025-04-13"),
+//   },
+// ];
 
 export default function FootballPage() {
-  return <VerticalTimeline initialReservations={reservations} title="⚽ Boisko do piłki nożnej" />;
+  return <VerticalTimeline title="FOOTBALL" />;
 }
 
